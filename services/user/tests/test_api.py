f62ac5c94e2391b37a56afcac1c5c6379e68fc2b
Yan Feng <141114704+kcornw@users.noreply.github.com>
2024-12-04 18:24:35 +0800

    test(user): add login & register tests

--- services/user/tests/test_api.py
+++ services/user/tests/test_api.py
@@ -1,6 +1,7 @@
 import json
 import unittest
 from src import create_app
+from src.model import User
 from src.extensions import db
 
 
@@ -23,6 +24,69 @@ class ApiTestCase(unittest.TestCase):
 
         self.app_context.pop()
 
+    def register(self, email=None, nickname=None, password=None):
+        """
+        Register a user.
+
+        Args:
+            email (str): The email of the user.
+            nickname (str): The nickname of the user.
+            password (str): The password of the user.
+
+        Returns:
+            dict: The response payload.
+        """
+
+        email = email or "test@email.email"
+        nickname = nickname or "test"
+        password = password or "test_password"
+        response = self.client.post(
+            "/api/user/register",
+            data=json.dumps(
+                {
+                    "email": email,
+                    "nickname": nickname,
+                    "password": password,
+                }
+            ),
+            content_type="application/json",
+        )
+
+        self.assertEqual(response.status_code, 200)
+        payload: dict = json.loads(response.data)
+
+        return payload
+
+    def login(self, email=None, password=None):
+        """
+        Login a user.
+
+        Args:
+            email (str): The email of the user.
+            password (str): The password of the user.
+
+        Returns:
+            dict: The response payload.
+        """
+
+        email = email or "test@email.email"
+        password = password or "test_password"
+        response = self.client.post(
+            "/api/user/login",
+            data=json.dumps(
+                {
+                    "email": email,
+                    "password": password,
+                }
+            ),
+            content_type="application/json",
+        )
+
+        self.assertEqual(response.status_code, 200)
+        payload: dict = json.loads(response.data)
+
+        return payload
+
     def test_app_exist(self):
         self.assertIsNotNone(self.app)
 
@@ -30,11 +94,15 @@ class ApiTestCase(unittest.TestCase):
         self.assertTrue(self.app.testing)
         self.assertTrue(self.app.config["TESTING"])
 
+    def test_nonexistent_url(self):
+        response = self.client.get("/api/user/nonexistent")
+        self.assertEqual(response.status_code, 404)
+
     def test_api_health(self):
         response = self.client.get("/api/user/health")
         self.assertEqual(response.status_code, 200)
 
-        payload = json.loads(response.data)
+        payload: dict = json.loads(response.data)
         self.assertIn("success", payload)
         self.assertIn("code", payload)
         self.assertIn("message", payload)
@@ -42,3 +110,61 @@ class ApiTestCase(unittest.TestCase):
 
         self.assertEqual(payload["success"], True)
         self.assertEqual(payload["code"], 0)
+
+    def test_api_login(self):
+        response = self.register()
+        response = self.client.post(
+            "/api/user/login",
+            data=json.dumps(
+                {
+                    "email": "test@email.email",
+                    "password": "test_password",
+                }
+            ),
+            content_type="application/json",
+        )
+        self.assertEqual(response.status_code, 200)
+
+        payload: dict = json.loads(response.data)
+        self.assertIn("success", payload)
+        self.assertIn("code", payload)
+        self.assertIn("message", payload)
+        self.assertIn("data", payload)
+
+        self.assertEqual(payload["success"], True)
+        self.assertEqual(payload["code"], 300)
+
+        user_id = payload["data"]["id"]
+        token = payload["data"]["token"]
+        user = User.get_by_id(user_id)
+        self.assertIsNotNone(user)
+        self.assertTrue(user.verify_token(token)[0])
+
+    def test_api_register(self):
+        response = self.client.post(
+            "/api/user/register",
+            data=json.dumps(
+                {
+                    "email": "test@email.email",
+                    "nickname": "test",
+                    "password": "test_password",
+                }
+            ),
+            content_type="application/json",
+        )
+        self.assertEqual(response.status_code, 200)
+
+        payload: dict = json.loads(response.data)
+        self.assertIn("success", payload)
+        self.assertIn("code", payload)
+        self.assertIn("message", payload)
+        self.assertIn("data", payload)
+
+        self.assertEqual(payload["success"], True)
+        self.assertEqual(payload["code"], 310)
+
+        user_id = payload["data"]["id"]
+        token = payload["data"]["token"]
+        user = User.get_by_id(user_id)
+        self.assertIsNotNone(user)
+        self.assertTrue(user.verify_token(token)[0])
