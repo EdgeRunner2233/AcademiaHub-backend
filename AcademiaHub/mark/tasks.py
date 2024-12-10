0a657153e0e19cb76185a92e52fede299f113b94
shushulail <141540543+shushulail@users.noreply.github.com>
2024-12-10 10:10:19 +0800

    1210

--- AcademiaHub/mark/tasks.py
+++ AcademiaHub/mark/tasks.py
@@ -2,10 +2,6 @@ from AcademiaHub.celery import app
 from .models import *
 from utils.search_utils import *
 
-@app.task
-def add_work_celery(work_id):
-    work = get_single_work(work_id)
-    if work:
-        work['']
+
 
 
