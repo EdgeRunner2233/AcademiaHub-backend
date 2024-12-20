c80c9c794f9614c25c17b3e748bae1f69bdb13d7
Yan Feng <141114704+kcornw@users.noreply.github.com>
2024-12-20 12:28:12 +0800

    feat: request url

---
+++ services/user/src/api_request.py
@@ -0,0 +1,26 @@
+import json
+import requests
+
+
+def request(url: str, method="GET", kwargs={}) -> dict:
+    """
+    Send request to url.
+
+    Args:
+        url (str): url to send request to
+        method (str, optional): request method. Defaults to "GET"
+        kwargs (dict, optional): request parameters. Defaults to {}
+
+    Raises:
+        RuntimeError: if request failed
+
+    Returns:
+        dict: response from api
+    """
+
+    res = requests.request(method, url, **kwargs)
+
+    if res.status_code != 200:
+        raise RuntimeError("Request failed")
+
+    return json.loads(res.text)
