9fe8248a0604b7b644f55189d1e33efe5df80a59
EdgeRunner2233 <1744724840@qq.com>
2024-12-21 23:13:19 +0800

    12212313

--- work/models.py
+++ work/models.py
@@ -30,3 +30,21 @@ class Work(models.Model):
             'openalex_id': self.openalex_id,
             'publication_year': self.publication_year.strftime('%Y-%m-%d') if self.publication_year else None,  # 格式化日期
         }
+
+
+class Literature(models.Model):
+    title = models.CharField(max_length=255)  # 标题
+    authors = models.CharField(max_length=1024)  # 作者（可以是多个作者，用逗号分隔）
+    abstract = models.TextField()  # 摘要
+    publish_text = models.CharField(max_length=255)  # 出版信息（可能是期刊名称、会议名称等）
+    year = models.PositiveIntegerField()  # 出版年份
+    publish = models.CharField(max_length=255)  # 出版方
+    ref_wr = models.PositiveIntegerField()  # 引用次数
+    key_words = models.CharField(max_length=512)  # 关键词（多个用逗号分隔）
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = "文献"
+        verbose_name_plural = "文献"
